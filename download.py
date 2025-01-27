bd1322ddfd8a18832150d0ecd390e50a3292de1b
Casey Traina <caseytraina@gmail.com>
2025-01-27 10:48:59 -0800

    all

---
+++ download.py
@@ -0,0 +1,10 @@
+from transformers import LlavaForConditionalGeneration, AutoTokenizer
+
+model_name = "omni-research/Tarsier-7b"
+
+# Download and save the model
+model = LlavaForConditionalGeneration.from_pretrained(model_name)
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+model.save_pretrained("model")
+tokenizer.save_pretrained("model")
