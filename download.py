45330db903fe08eabf092a8d3bd45835a6f38af7
Casey Traina <caseytraina@gmail.com>
2025-01-27 11:53:37 -0800

    all

--- download.py
+++ download.py
@@ -6,5 +6,5 @@ model_name = "omni-research/Tarsier-7b"
 model = LlavaForConditionalGeneration.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
-model.save_pretrained("model")
+model.save_pretrained("model", max_shard_size="2GB")
 tokenizer.save_pretrained("model")
